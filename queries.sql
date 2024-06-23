8043f6a512526ebd4e098c55d45fcc82d4344a57
leorev01 <eric@example.com>
2024-06-23 18:01:30 +0100

    Sorted queries.sql

--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
 CREATE TABLE items (
   id SERIAL PRIMARY KEY,
-  title VARCHAR(100) NOT NULL
+  title VARCHAR(100) NOT NULL,
+  date DATE NOT NULL
 );
 
-INSERT INTO items (title) VALUES ('Buy milk'), ('Finish homework');
+INSERT INTO items (title, date) VALUES ('Buy milk', '23/06/2024'), ('Finish homework', '23/06/2024');
